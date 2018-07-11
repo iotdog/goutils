7a7defbd267508e53598eef735fd4524e08268c9
River Wang <jiang.w@vip.163.com>
2018-07-11 10:40:55 +0800

    * 增加返回索引

--- array.go
+++ array.go
@@ -1,11 +1,11 @@
 package goutils
 
-// StringInSlice 检查字符串是否存在于列表中，https://stackoverflow.com/questions/15323767/does-golang-have-if-x-in-construct-similar-to-python
-func StringInSlice(a string, list []string) bool {
-	for _, b := range list {
+// StringInSlice 检查字符串是否存在于列表中，返回是否存在标识，若存在则返回所在索引，https://stackoverflow.com/questions/15323767/does-golang-have-if-x-in-construct-similar-to-python
+func StringInSlice(a string, list []string) (bool, int) {
+	for i, b := range list {
 		if b == a {
-			return true
+			return true, i
 		}
 	}
-	return false
+	return false, -1
 }
